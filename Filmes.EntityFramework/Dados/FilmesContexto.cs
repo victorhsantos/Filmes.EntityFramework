9cf9f1a7c96a9869f8ed74ad4b3df3dd59730ab7
Victor Hugo <vhos08@gmail.com>
2022-02-01 16:42:20 -0300

    Adicionar arquivos de projeto.

---
+++ Filmes.EntityFramework/Dados/FilmesContexto.cs
@@ -0,0 +1,15 @@
+﻿using Filmes.EntityFramework.Negocio;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filmes.EntityFramework.Dados
+{
+    public class FilmesContexto : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AluraFilmes;Trusted_connection=true;");
+        }
+    }
+}
---
+++ Filmes.EntityFramework/Program.cs
@@ -0,0 +1,23 @@
+﻿using Filmes.EntityFramework.Dados;
+using Filmes.EntityFramework.Extensions;
+using System;
+
+namespace Filmes.EntityFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //select * from actor
+            using (var contexto = new FilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                foreach (var ator in contexto.Atores)
+                {
+                    System.Console.WriteLine(ator);
+                }
+            }
+        }
+    }
+}
